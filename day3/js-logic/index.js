f3e9ab24b0daf077f9ab54ffd4669d3e01b2d0eb
Kaifi@01 <kaifimallick2001@gmail.com>
2026-05-08 23:10:08 +0530

    Add JavaScript string and array problem solving

---
+++ day3/js-logic/index.js
@@ -0,0 +1,52 @@
+//1 reverse a string
+// let name="kaifi";
+// let name1=""
+// for (let index = name.length-1; index >= 0; index--) {
+//   name1+=name[index]
+    
+// }
+// console.log(name1);
+//2 find max number from array
+// let numbers=[20,10,50,99,19];
+
+// let max=numbers[0];
+// for (let index = 0; index < numbers.length; index++) {
+//      if ( max < numbers[index]) {
+//          max=numbers[index]
+//      }
+    
+// }
+// console.log(max);
+//3 count vowels in string
+
+// let name="kaifi";
+// let vow=['A','E','I','O','U','a','e','i','o','u'];
+//  let count=0;
+// for (let index = 0; index < name.length; index++) {
+//     if (vow.includes(name[index])) {
+//           count++ 
+//     }
+    
+// }
+// console.log(count);
+// //4 check palindrome
+//  function palindrome(name){
+//     let res1=name.toUpperCase();
+//     let res=res1.split("").reverse().join("");
+//     return res1===res
+   
+    
+//  }
+// console.log(palindrome("madam"));
+// 5 factorial using loop
+//  let num=5;
+//  let res=1;
+// for (let index = num; index > 0;  index--) {
+//     res=res*index
+
+   
+    
+   
+// }
+
+// console.log(res); 
